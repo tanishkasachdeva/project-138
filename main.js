4b93938df10e4ba69860e0b7a09f59f9932c8b19
tanishkasachdeva <73342817+tanishkasachdeva@users.noreply.github.com>
2022-12-30 15:09:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,12 +23,26 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  video=createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  poseNet=ml5.poseNet(video,modelLoaded);
+  
+}
+
+function modelLoaded()
+{
+  console.log("model is loaded");
 }
 
 
 function draw(){
 
- background(0); 
+ background(0);
+ 
+ image(video,0,0,700,600);
 
  fill("black");
  stroke("black");
